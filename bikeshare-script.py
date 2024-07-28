61a644d1b34aefc5490474d786e3e9144928574f
Ashton_Murray <ashton_mitchell@hotmail.com>
2024-07-28 12:23:55 +1000

    Added clarifying information in docstring for the load_data(city,month,day) function

--- bikeshare-script.py
+++ bikeshare-script.py
@@ -55,7 +55,7 @@ def load_data(city, month, day):
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
+        DataFrame - Pandas DataFrame containing city data filtered by month and day if applicable, or none if the file is not found.
     """
     
     months = ['all', 'january', 'february', 'march', 'april', 'may']
